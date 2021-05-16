593a5d4724f3a9845a8e2c3f45781d463e8e5c8b
harutiro <harutiro2027@outlook.jp>
2021-05-16 14:46:42 +0900

    Initial commit

---
+++ app/src/main/java/app/makino/harutiro/ReceivingSimpleDataTest/MainActivity.kt
@@ -0,0 +1,38 @@
+package app.makino.harutiro.ReceivingSimpleDataTest
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.Picasso
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var text = "https://twitter.com/home"
+
+
+        findViewById<TextView>(R.id.textView).text = text
+
+
+        val domein = UrlDomein().hen(text)
+
+
+        findViewById<TextView>(R.id.textView2).text = domein
+
+
+
+        Picasso.get()
+            //画像が乗っているURL
+            .load("https://www.google.com/s2/favicons?domain=$domein")
+            .resize(300, 300) //表示サイズ指定
+            .centerCrop() //resizeで指定した範囲になるよう中央から切り出し
+            .into(findViewById<ImageView>(R.id.imageView)) //imageViewに流し込み
+
+
+    }
+}
